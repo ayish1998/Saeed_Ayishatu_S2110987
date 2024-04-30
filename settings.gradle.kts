17a35da3a956a4cd38bb1d9462a383b683f482e4
Ayishatu Saeed <91335764+ayish1998@users.noreply.github.com>
2024-04-30 03:47:40 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Saeed_Ayishatu_S2110987"
+include(":app")
+ 
